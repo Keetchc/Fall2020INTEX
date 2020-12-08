51075988f292c2bf9638f17c9aed8e83b828e311
Keetchc <70861267+Keetchc@users.noreply.github.com>
2020-12-07 17:53:00 -0700

    mostly formatting changes, starting to come together!

--- company/urls.py
+++ company/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import jobPageView, jobListPageView
+from .views import jobListPageView
 
 urlpatterns = [
-    path('job/', jobPageView, name='job'),
     path('', jobListPageView, name='joblist'),
 ]
