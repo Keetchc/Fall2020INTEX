ef0b7bbc098bd9e5ae215bd99c1447029be2ff61
Keetchc <70861267+Keetchc@users.noreply.github.com>
2020-12-09 08:49:32 -0700

    Fixed recommender and made the pathing make sense.

--- company/urls.py
+++ company/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import jobListPageView, viewApplicantsPageView, postAJobPageView, storeAJobPageView
+from .views import jobListPageView, viewApplicantsPageView, postAJobPageView, storeAJobPageView, jobPageView, companyJobListPageView, companyJobPageView
 
 urlpatterns = [
     path('', jobListPageView, name='joblist'),
+    path('job/', jobPageView, name='job'),
+    path('companyjob/', companyJobPageView, name='companyjob'),
+    path('companyjobs/', companyJobListPageView, name='companyjoblist' ),
     path('viewapplicants/', viewApplicantsPageView, name='applicantlist'),
     path('postajob/', postAJobPageView, name='postajob'),
     path('storeajob/', storeAJobPageView, name='storeajob')
